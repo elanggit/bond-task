1f2d6ab732f223280665a35d23673ffd8a09b014
elanggit <elanggithub@gmail.com>
2024-10-10 23:30:02 -0400

    Comment routes

--- simple-server/routes/index.js
+++ simple-server/routes/index.js
@@ -4,7 +4,9 @@ import playerController from '../controllers/playerController';
 const router = express.Router();
 
 router.get('/', playerController.getPlayers);
-router.get('/', playerController.getPlayers);
+// router.get('/', playerController.getPlayers);
 
-
+// router.get('/getFavorites', playerController.getFavoritePlayers);
+// router.post('/', playerController.addFavoritePlayer);
+// router.post('/', playerController.removeFavoritePlayer);
 module.exports = router;
